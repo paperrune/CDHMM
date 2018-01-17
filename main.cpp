a54f4682ea7ff3f6e0560db20881749475b04b6a
paperrune <paperrune@naver.com>
2018-01-17 20:39:11 +0900

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,3 +1,4 @@
+#include <hash_map>
 #include <omp.h>
 #include <stdio.h>
 #include <stdlib.h>
@@ -5,6 +6,8 @@
 #include <time.h>
 
 #include "CHMM.h"
+
+using namespace stdext;
 
 void Read_MNIST(char training_set_images[], char training_set_labels[], char test_set_images[], char test_set_labels[], int time_step, int number_training, int number_test, int *label, double ***input){
 	FILE *file;
@@ -84,15 +87,15 @@ void Read_MNIST(char training_set_images[], char training_set_labels[], char tes
 
 int main(){
 	char *state_label[] = { "0", "0", "0", "1", "1", "1", "2", "2", "2", "3", "3", "3", "4", "4", "4", "5", "5", "5", "6", "6", "6", "7", "7", "7", "8", "8", "8", "9", "9", "9", "-", "-", "-" };
-	char type_covariance[]	= "diagonal";	// <-> "full"
-	char type_model[]		= "bakis";		// <-> "ergodic"
+	char type_covariance[] = "diagonal";	// <-> "full"
+	char type_model[] = "bakis";		// <-> "ergodic"
 
-	int dimension_event		= 56;
-	int length_data			= 14;
-	int number_iterations	= 100;
-	int number_threads		= 4;
-	int number_training		= 60000;
-	int number_test			= 10000;
+	int dimension_event = 56;
+	int length_data = 14;
+	int number_iterations = 100;
+	int number_threads = 4;
+	int number_training = 60000;
+	int number_test = 10000;
 
 	int number_gaussian_components = 1;
 
@@ -117,22 +120,25 @@ int main(){
 
 	// Construct CHMM
 	{
-		bool **state_connection = new bool*[sizeof(state_label) / sizeof(state_label[0])];
+		hash_map<int, bool> *state_connection = new hash_map<int, bool>[sizeof(state_label) / sizeof(state_label[0])];
 
 		int number_states = sizeof(state_label) / sizeof(state_label[0]);
 
 		for (int i = 0; i < number_states; i++){
-			state_connection[i] = new bool[number_states];
-
 			for (int j = 0; j < number_states; j++){
-				// state_connection[i][j] = 1; // for ergodic model
-				state_connection[i][j] = (i == j || (i % 3 == 2 && j % 3 == 0) || j - i == 1);
+				// for ergodic model
+				// state_connection[i].insert(hash_map<int, bool>::value_type(j, true));
+
+				// for bakis model
+				if (i == j || (i % 3 == 2 && j % 3 == 0) || j - i == 1){
+					state_connection[i].insert(hash_map<int, bool>::value_type(j, true));
+				}
 			}
 		}
 		CHMM = new Continuous_Hidden_Markov_Model(state_connection, type_covariance, type_model, state_label, dimension_event, number_gaussian_components, number_states);
 
 		for (int i = 0; i < number_states; i++){
-			delete[] state_connection[i];
+			state_connection[i].clear();
 		}
 		delete[] state_connection;
 	}
@@ -175,8 +181,8 @@ int main(){
 				#pragma omp atomic
 				number_correct[(i < number_training) ? (0) : (1)] += (atoi(&optimal_label_sequence[2]) == label[i]);
 
-				if(optimal_label_sequence) delete[] optimal_label_sequence;
-				delete[] optimal_state_sequence;
+				if (optimal_label_sequence) delete[] optimal_label_sequence;
+				if (optimal_state_sequence) delete[] optimal_state_sequence;
 			}
 			printf("score: %d / %d, %d / %d  L: %lf  step %d  %.2lf sec\n", number_correct[0], number_training, number_correct[1], number_test, log_likelihood, h + 1, (double)(clock() - time) / CLOCKS_PER_SEC);
 		}
